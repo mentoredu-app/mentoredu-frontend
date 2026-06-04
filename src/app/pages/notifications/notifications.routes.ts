f1ac3691b3b463e09ec0fe15178283831d827837
Ivan Cunyas <ivanruben.cr@gmail.com>
2026-06-04 01:40:43 -0500

    feat: notificaciones

--- src/app/pages/notifications/notifications.routes.ts
+++ src/app/pages/notifications/notifications.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NotificationsList } from './notifications-list/notifications-list';
 
-export const NOTIFICATIONS_ROUTES: Routes = [];
+export const NOTIFICATIONS_ROUTES: Routes = [
+  { path: '', component: NotificationsList },
+];
